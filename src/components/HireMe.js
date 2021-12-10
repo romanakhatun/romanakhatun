914e73c3c54ae5e08e494b669391aa86673cd25c
romanakhatun <romanakhatun2004@gmail.com>
2021-12-10 14:09:34 +0600

    change

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -1,10 +1,16 @@
+import { Container } from "@material-ui/core";
+import { HiBadgeCheck } from "react-icons/hi";
 import styles from "../styles/components/hireMe.module.scss";
 const HireMe = () => {
   return (
-    <div className={styles.hireMe}>
-      <h2>I'm available to start working for you!</h2>
-      <button>Hire Me</button>
-    </div>
+    <Container>
+      <div className={styles.hireMe}>
+        <h2>I'm available to start working for you!</h2>
+        <button>
+          <HiBadgeCheck /> Hire Me!
+        </button>
+      </div>
+    </Container>
   );
 };
 export default HireMe;
